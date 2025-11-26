c056db664d37f40813918788b46f6ef380c8ff1e
TDFThomasN <168515715+TDFThomasN@users.noreply.github.com>
2025-11-26 10:42:39 +0100

    Initial

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,12 +1,23 @@
 import { Component, signal } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { RouterOutlet, RouterLink, RouterLinkActive } from '@angular/router';
+import { Home } from './home/home';
+import { About } from './about/about';
 
-@Component({
+@Component({​
   selector: 'app-root',
-  imports: [RouterOutlet],
-  templateUrl: './app.html',
-  styleUrl: './app.css'
+  imports: [RouterOutlet, RouterLink, RouterLinkActive, Home, About],​
+  template: `​
+    <nav style="padding:.5rem; border-bottom:1px solid #ddd;">​
+      <a routerLink="/" routerLinkActive="active" [routerLinkActiveOptions]="{ exact: true }">Accueil</a> |​
+      <a routerLink="/about" routerLinkActive="active">À propos</a>​
+    </nav>​
+​
+    <main style="padding:1rem;">​
+      <router-outlet></router-outlet>
+    </main>
+  `
 })
+
 export class App {
   protected readonly title = signal('TaskBoard_Pro');
 }
