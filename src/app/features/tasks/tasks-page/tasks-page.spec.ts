e515b034a8608f053a0c31da9f342bed1e683499
TDFThomasN <168515715+TDFThomasN@users.noreply.github.com>
2026-01-23 09:03:58 +0100

    reset

--- src/app/features/tasks/tasks-page/tasks-page.spec.ts
+++ src/app/features/tasks/tasks-page/tasks-page.spec.ts
@@ -1,19 +1,1 @@
-import { TestBed } from '@angular/core/testing';
-
-import { Task } from '../../../core/services/task';
-
-describe('Task', () => {
-  let service: Task;
-
-  beforeEach(() => {
-    TestBed.configureTestingModule({});
-    service = TestBed.inject(Task);
-  });
-
-  it('should be created', () => {
-    expect(service).toBeTruthy();
-  });
-});
-
-
 
